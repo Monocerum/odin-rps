094ac7bb69df24629de0665d2bd6898ec56e35e8
Monocerum <monoceruscaelum@gmail.com>
2023-09-14 17:32:00 +0800

    Add UI to project

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function getPlayerChoice() {
-    let playerSelection = (prompt("Rock, paper, or scissors? ")).toLowerCase();
-    console.log("Player Choice: " + playerSelection);
-    return playerSelection;
+function getPlayerChoice(e) {
+    const clicked = e.target.id;
+
+    return clicked;
 }
+
 
 function getComputerChoice() {
     let computerChoices = ["rock", "paper", "scissors"];
 
     let randomNum = (Math.floor(Math.random() * computerChoices.length) + 1);
-    console.log(randomNum);
 
     if (randomNum === 1) {
         return "rock";
@@ -19,63 +19,71 @@ function getComputerChoice() {
     }
 }
 
-// Plays five rounds of rock-paper-scissors
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissors!";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beat paper!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats rock!";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors!";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock!";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beat paper!";
-    }
-}
 
-// Function to play five rounds of rock, paper, scissors
-function playGame() {
-    let playerWin = 0;
-    let computerWin = 0;
+let roundCount = 1;
+let wins = 0;
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = getPlayerChoice();
-
-        // Check if player input is valid, else, try again
-        if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-            alert("Invalid option! Please try again.");
-            playerSelection = getPlayerChoice();
-        }
-
-        let computerSelection = getComputerChoice();
-        console.log("Computer Choice: " + computerSelection);
-        
-        let winner = playRound(playerSelection, computerSelection);
-        console.log(winner);
-
-        // Increment count for player wins and computer wins
-        if (winner.includes("win", 0)) {
-            playerWin++;
-        } else if (winner.includes("lose", 0)) {
-            computerWin++;
-        } else if (winner.includes("tie"), 0) {
-            playerWin + 0.5;
-            computerWin + 0.5;
-        }
+function playRound(e) {
+    if (roundCount > 5) {
+        alert("You've already played 5 rounds!");
+        return;
     }
 
-    if (playerWin > computerWin) {
-        alert("Player wins!");
-    } else if (computerWin > playerWin) {
-        alert("Computer wins! Better luck next time!");
+    let playerChoice = getPlayerChoice(e);
+    let computerChoice = getComputerChoice();
+
+    const results = document.querySelector('.results');
+    const roundResult = document.createElement('p');
+    const roundNum = document.createElement('h3');
+    const finalRound = document.createElement('h1');
+    const finalResult = document.createElement('h4');
+
+    results.appendChild(roundNum);
+    results.appendChild(roundResult);
+    results.appendChild(finalRound);
+    results.appendChild(finalResult);
+
+    roundResult.classList.add('round-outcome');
+    roundNum.classList.add('round-count');
+    finalRound.classList.add('final-round');
+    finalResult.classList.add('final-outcome');
+
+    if (playerChoice === computerChoice) {
+        roundNum.textContent = `Round ${roundCount}`;
+        roundResult.textContent = "It's a tie!";
+        wins = wins + 0.5;
+    } else if ((playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "paper") || 
+        (playerChoice === "paper" && computerChoice === "rock")) {
+        roundNum.textContent = `Round ${roundCount}`;
+        roundResult.textContent = `You win! Your choice, ${playerChoice}, beats computer choice, ${computerChoice}!`;
+        wins++;
     } else {
-        alert("It's a tie!");
+        roundNum.textContent = `Round ${roundCount}`;
+        roundResult.textContent = `You lose! Computer choice, ${computerChoice}, beats your choice, ${playerChoice}!`;
+    };
+
+    if (roundCount === 5) {
+        finalRound.textContent = "Final Result!";
+
+        if (wins > 2.5) {
+            finalResult.textContent = "You won the game!";
+        }
+        else if (wins < 2.5) {
+            finalResult.textContent = "Computer won the game! Better luck next time";
+        } else {
+            finalResult.textContent = "It's a tie!";
+        }
     }
+
+    roundCount++;
 }
 
-playGame();
+document.addEventListener('DOMContentLoaded', function() {
+    const options = document.querySelectorAll('button');
+    console.log(options);
+
+    options.forEach(option => {
+        option.addEventListener('click', playRound);
+    });
+});
